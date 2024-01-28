7ae24a2f1ab2a42f1e2d437b9042496b8b7ebf56
Galaxygon <51724830+Galaxygon666@users.noreply.github.com>
2024-01-28 01:39:47 +0100

    Project setup
    Added MakeFile
    Linked includes in VSCODE (Change includePath in .vscode c_cpp_properties.json)

---
+++ includes/ClassDFirmware.h
@@ -0,0 +1,4 @@
+// Define MCU 
+#ifndef __AVR_ATmega328P__
+    #define __AVR_ATmega328P__
+#endif
---
+++ src/ClassDFirmware.c
@@ -0,0 +1,13 @@
+// Custom libraries
+#include <includes/ClassDFirmware.h>
+
+// AVR libraries
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+
+    return 0;
+}
